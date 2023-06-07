1c08b394de4be6c1f7a03b1fc59020d6b5efb007
shane-kercheval <shane.kercheval@gmail.com>
2023-06-07 02:36:26 +0000

    Refactor

--- source/entrypoints/app.py
+++ source/entrypoints/app.py
@@ -1,123 +1,34 @@
-# import streamlit as st
-
-# def main():
-#     state = st.session_state.setdefault('state', {'text_area_b': ''})
-#     # state = {'text_area_b': ''}
-#     print(state)
-
-#     text_area_a = st.text_area("Enter text for TextArea A")
-#     button_a = st.button("Button A")
-
-#     print(f"text_area_a - 1: `{text_area_a}`")
-#     if button_a:
-#         # Update contents of TextArea B with contents of TextArea A
-#         print("updating state")
-#         state['text_area_b'] = text_area_a
-#         print(f"state: `{state['text_area_b']}`")
-
-#     print(f"state - 2: `{state['text_area_b']}`")
-#     text_area_b = st.text_area("TextArea B", value=state['text_area_b'], key='text_area_b')
-#     print(f"text_area_b - 1: `{text_area_b}`")
-
-#     button_b = st.button("Button B")
-#     if button_b:
-#         print(f"text_area_b - 2: `{text_area_b}`")
-#         st.write("Contents of TextArea B:")
-#         st.write(text_area_b)
-#         state['text_area_b'] = text_area_b  # Update session state value
-
-#     print("-----end------")
-
-# if __name__ == "__main__":
-#     main()
-
-
+"""
+Streamlit app that enables conversations with ChatGPT and additional features like prompt
+templates.
+"""
+import os
+from dotenv import load_dotenv
 import streamlit as st
-#Known issues;
-#Once you modify the chat message, the prompt template message will know fill the text-box
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import HumanMessage, AIMessage
+import source.library.streamlit_helpers as sh
+
+# TODO: document
+# Known issues;
+# Once you modify the chat message, the prompt template message will know fill the text-box
 # Steps: fill out fields in prompt template; hit "Create Message"; Chat message is updated;
 # change one or more fields; hit "Create Message"; Chat message is still updated; Modify chat
 # message; now click "Create Message" from prompt template; Chat message will not update;
 # however, if you change one or more of field values in the prompt template and hit Create Message
 # then the chat message will update as expected.
 
-    # setup streamlit page
 st.set_page_config(
         page_title="ChatGPT",
         page_icon="🤖",
         layout='wide',
     )
 
-def display_horizontal_line():
-    css = """
-    <style>
-        .line {
-            border: none;
-            border-top: 1px solid #E6E6E6;
-            margin: 10px 0;
-        }
-    </style>
-    """
-    st.markdown(css, unsafe_allow_html=True)
-    st.markdown("<div class='line'></div>", unsafe_allow_html=True)
 
-
-def display_chat_message(message, is_sender):
-    sender_emoji = "👨‍💼" if is_sender else "🤖"
-    sender_class = "sender" if is_sender else "receiver"
-
-    col1, col2 = st.columns([1, 20])
-    with col1:
-        st.markdown(f"<div class='emoji'>{sender_emoji}</div>", unsafe_allow_html=True)
-    with col2:
-        st.markdown(f"<div class='{sender_class}'>{message}</div>", unsafe_allow_html=True)
-
-    # st.markdown(f"<div class='{sender_class}'><span class='emoji'>{sender_emoji}</span> {message}</div>", unsafe_allow_html=True)
-    # st.markdown(f"<div class='{sender_class}'>{message}</div>", unsafe_allow_html=True)
-
-# # Sample messages
-# message = """
-# This is some python
-
-# ```
-# def python():
-#     return True
-# ```
-# """
-# messages = [
-#     {"sender": "John", "message": "asdf"},
-#     {"sender": "Jane", "message": message},
-#     {"sender": "John", "message": "I'm good, thanks!"},
-#     {"sender": "Jane", "message": "That's great to hear!"},
-#     {"sender": "John", "message": "Yeah, it's been a good day."},
-# ]
-
-# # Displaying the chat messages
-# for message in reversed(messages):
-#     display_chat_message(message["message"], message["sender"] == "John")
-#     # display_horizontal_line()
-
-
-
-# python 3.8 (3.8.16) or it doesn't work
-# pip install streamlit streamlit-chat langchain python-dotenv
-import streamlit as st
-from streamlit_chat import message
-from dotenv import load_dotenv
-import os
-
-from langchain.chat_models import ChatOpenAI
-from langchain.schema import (
-    SystemMessage,
-    HumanMessage,
-    AIMessage,
-)
-
-
-def init():
+def initialize() -> None:
+    """Initialize environment and app."""
     # Load the OpenAI API key from the environment variable
     load_dotenv()
-    
     # test that the API key exists
     if os.getenv("OPENAI_API_KEY") is None or os.getenv("OPENAI_API_KEY") == "":
         print("OPENAI_API_KEY is not set")
@@ -126,80 +37,14 @@ def init():
         print("OPENAI_API_KEY is set")
 
 
-
-message = """
-This is some python:
-
-```
-def python():
-    return True
-```
-
-It is code.
-"""
-
-def main():
-    init()
-
-    css = ''
-    # css += '<style> .main { padding: 20px; }</style>'
-    # css for chat message
-    css += """
-    <style>
-    section.main .block-container {
-        padding-top: 30px;
-        padding-bottom: 20px;
-    }
-    </style>
-    <style>
-        .sender {
-            background-color: #d8f0ff;
-            padding: 20px;
-            border-radius: 5px;
-            margin-bottom: 30px;
-        }
-        .receiver {
-            background-color: #F0F0F0;
-            padding: 20px;
-            border-radius: 5px;
-            margin-bottom: 10px;
-        }
-        .emoji {
-            font-size: 24px;
-            margin-bottom: 5px;
-        }
-        .stCodeBlock {
-            margin: 20px 0;
-        }
-    </style>
-    """
-    # css for sidebar width
-    css += '<style>section[data-testid="stSidebar"]{width: 400px !important;}</style>'
-    # custom style for text box e.g. prompt-template in sidebar
-    css += """
-    <style>
-    div[data-testid="stText"] {
-        background-color: white;
-        padding: 10px;
-        white-space: pre-wrap;
-        width: 100%;
-    }
-    </style>
-    """
-     # Add custom CSS to hide the label of the prompt-template selection box in the sidebar
-    css += """
-    <style>
-    section[data-testid="stSidebar"] .stSelectbox label {
-        display: none;
-    }
-    </style>
-    """
-    # Add custom CSS to hide the label of the chat message
-    css += '<style>section.main div.stTextArea label { display: none;}</style>'
-    st.markdown(css, unsafe_allow_html=True)
-
-    state = st.session_state.setdefault('state', {'chat_message': ''})
-    print(f"state: {state}")
+def main() -> None:
+    """Defines the application structure and behavior."""
+    initialize()
+    sh.apply_css()
+    message_state = st.session_state.setdefault('state', {'chat_message': ''})
+    # initialize message history
+    if "messages" not in st.session_state:
+        st.session_state.messages = sh._return_mock_conversation()
 
     with st.sidebar:
         st.markdown('# Options')
@@ -218,93 +63,35 @@ def main():
             )
 
         st.markdown('# Prompt Template')
-
-        template_option = st.selectbox(
-            '<do not display>',
-            (
-                '<Select>',
-                'Make text sound better.',
-                'Summarize text.',
-                'Code: Write doc strings',
-            ),
-        )
-        if template_option != '<Select>':
-            prompt_example = """
-            This is a prompt.
-
-            It has fields like this {{context}}
-
-            And this:
-
-            ```
-            {{more_context}}
-            ```
-            """
-
-            # Regular expression pattern to match values within double brackets
-            pattern = r"\{\{(.*?)\}\}"
-
-            import re
-            # Find all matches of the pattern in the text
-            matches = re.findall(pattern, prompt_example, re.DOTALL)
-
-            # Create a dictionary to store the extracted values
-            prompt_field_values = []
-
-            # Create the sidebar text areas
-            with st.form(key="my_form"):
+        template_name = sh.create_prompt_template_options()
+        if template_name != '<Select>':
+            prompt_template = sh.get_prompt_template(template_name=template_name)
+            template_fields = sh.get_fields_from_template(prompt_template=prompt_template)
+            field_values = []
+            # use form to prevent text_areas from refreshing app after focus moves away
+            with st.form(key="prompt_template_fields_form"):
                 st.markdown("### Fields")
-                st.markdown("Fill in the information for the following fields referred to in the prompt template, shown below.")
-                for match in matches:
-                    prompt_field_values.append((match, st.text_area(match, height=100, key=match)))
-                    # prompt_field_values.append((match, st.sidebar.text_area(match, height=100)))
+                st.markdown("Fill in the information for the following fields referred to in the prompt template, shown below.")  # noqa
+                for field in template_fields:
+                    field_values.append((field, st.text_area(field, height=100, key=field)))
+                    # field_values.append((match, st.sidebar.text_area(match, height=100)))
+
+                # when this button is pressed, we will build up the message from the template
+                # and the values from the user and fill in the chat message text_area
                 create_message_submit = st.form_submit_button(label='Create message')
 
-            # Create the button to extract the text
             if create_message_submit:
-                # extracted_values = {key: values_dict[key] for key in values_dict if values_dict[key]}
+                chat_gpt_message = prompt_template
+                # replace all instances of `{{field}}` with the value from the user
+                for field, value in field_values:
+                    chat_gpt_message = chat_gpt_message.replace("{{" + field + "}}", value)
 
-                chat_gpt_message = prompt_example.strip()
-                for key, value in prompt_field_values:
-                    chat_gpt_message = chat_gpt_message.replace("{{" + key + "}}", value)
-                
-                    # Update contents of TextArea B with contents of TextArea A
-                print("updating chat_message state")
-                state['chat_message'] = chat_gpt_message
-                print(f"state - 2: `{state}`")
-                
-
-                # st.sidebar.write("Extracted Values:")
-                # st.sidebar.write(extracted_values)
+                message_state['chat_message'] = chat_gpt_message
+                print(f"message_state - updated: `{message_state}`")
 
             st.markdown('### Template:')
-            st.sidebar.text(prompt_example)
+            st.sidebar.text(prompt_template)
             # st.sidebar.markdown(f'<div class="sidebar-text">{prompt_example}</div>', unsafe_allow_html=True)
-
-
-    # initialize message history
-    if "messages" not in st.session_state:
-        st.session_state.messages = [
-            SystemMessage(content="You are a helpful assistant."),
-            HumanMessage(content="What is the capital of France?"),
-            AIMessage(content='The capital of France is Paris.', additional_kwargs={}, example=False),  # noqa
-            HumanMessage(content="What is 1 + 1?"),
-            AIMessage(content='2', additional_kwargs={}, example=False),  # noqa
-            HumanMessage(content="What is 2 + 2?"),
-            AIMessage(content='4', additional_kwargs={}, example=False),  # noqa
-            HumanMessage(content="What is the capital of France?"),
-            AIMessage(content='The capital of France is Paris.', additional_kwargs={}, example=False),  # noqa
-            HumanMessage(content="What is 1 + 1?"),
-            AIMessage(content='2', additional_kwargs={}, example=False),  # noqa
-            HumanMessage(content="What is 2 + 2?"),
-            AIMessage(content='4', additional_kwargs={}, example=False),  # noqa
-            HumanMessage(content="What is the capital of France?"),
-            AIMessage(content='The capital of France is Paris.', additional_kwargs={}, example=False),  # noqa
-            HumanMessage(content="What is 1 + 1?"),
-            AIMessage(content='2', additional_kwargs={}, example=False),  # noqa
-            HumanMessage(content="What is 2 + 2?"),
-            AIMessage(content=message, additional_kwargs={}, example=False),  # noqa
-        ]
 
 
     print("A")
@@ -314,7 +101,7 @@ def main():
         user_input = st.text_area(
             "Send a message.",
             key='chat_message',
-            value=state['chat_message'],
+            value=message_state['chat_message'],
             placeholder="Ask a question.",
             height=150,
         )
@@ -335,8 +122,7 @@ def main():
         """
         st.markdown(cost_html, unsafe_allow_html=True)
 
-    
-    display_horizontal_line()
+    sh.display_horizontal_line()
     # handle user input
     if submit_button and user_input:
         print("B")
@@ -359,8 +145,8 @@ def main():
             chat = ChatOpenAI(model=model_name, temperature=temperature, max_tokens=max_tokens)
             response = chat([st.session_state.messages[0]] + [st.session_state.messages[-1]])
         st.session_state.messages.append(response)
-        state['chat_message'] = user_input  # Update session state value
-        print(f"updating state - 2: {state}")
+        message_state['chat_message'] = user_input  # Update session state value
+        print(f"updating state - 2: {message_state}")
 
 
     print("C")
@@ -379,8 +165,8 @@ def main():
         assert isinstance(ai_message, AIMessage)
         col_1, col_2 = st.columns([5, 1])
         with col_1:
-            display_chat_message(ai_message.content, is_sender=False)
-            display_chat_message(human_message.content, is_sender=True)
+            sh.display_chat_message(ai_message.content, is_human=False)
+            sh.display_chat_message(human_message.content, is_human=True)
         with col_2:
             cost_string = f"""
             <b>Message Cost</b>: <code>$1.00</code><br>
---
+++ source/library/streamlit_helpers.py
@@ -0,0 +1,220 @@
+"""Helper functions for streamlit app."""
+import re
+import streamlit as st
+from langchain.schema import BaseMessage, SystemMessage, HumanMessage, AIMessage
+
+def apply_css() -> None:
+    """Applies css to the streamlit app."""
+    css = ''
+    # css for main container
+    css += """
+    <style>
+    section.main .block-container {
+        padding-top: 30px;
+        padding-bottom: 20px;
+    }
+    </style>
+    """
+    # css for chat message
+    css += """
+    <style>
+        .sender {
+            background-color: #d8f0ff;
+            padding: 20px;
+            border-radius: 5px;
+            margin-bottom: 30px;
+        }
+        .receiver {
+            background-color: #F0F0F0;
+            padding: 20px;
+            border-radius: 5px;
+            margin-bottom: 10px;
+        }
+        .emoji {
+            font-size: 24px;
+            margin-bottom: 5px;
+        }
+        .stCodeBlock {
+            margin: 20px 0;
+        }
+    </style>
+    """
+    # css for sidebar width
+    css += '<style>section[data-testid="stSidebar"]{width: 400px !important;}</style>'
+    # custom style for text box e.g. prompt-template in sidebar
+    css += """
+    <style>
+    div[data-testid="stText"] {
+        background-color: white;
+        padding: 10px;
+        white-space: pre-wrap;
+        width: 100%;
+    }
+    </style>
+    """
+     # Add custom CSS to hide the label of the prompt-template selection box in the sidebar
+    css += """
+    <style>
+    section[data-testid="stSidebar"] .stSelectbox label {
+        display: none;
+    }
+    </style>
+    """
+    # Add custom CSS to hide the label of the chat message
+    css += '<style>section.main div.stTextArea label { display: none;}</style>'
+    st.markdown(css, unsafe_allow_html=True)
+
+
+def display_horizontal_line(margin: str = '10px 0px 30px 0px') -> None:
+    """
+    Displays a display_horizontal_line.
+
+    Args:
+        margin: margin in pixels
+    """
+    css = f"""
+    <style>
+        .line {{
+            border: none;
+            border-top: 1px solid #E6E6E6;
+            margin: {margin};
+        }}
+    </style>
+    """
+    st.markdown(css + "<div class='line'></div>", unsafe_allow_html=True)
+
+
+def display_chat_message(message: str, is_human: bool) -> None:
+    """
+    Displays a chat message and formats according to whether or not the message is from a human.
+
+    Args:
+        message: the message to display
+        is_human: True if the message is from a human; False if message is OpenAI response
+    """
+    sender_emoji = "👨‍💼" if is_human else "🤖"
+    sender_class = "sender" if is_human else "receiver"
+
+    col1, col2 = st.columns([1, 20])
+    with col1:
+        st.markdown(f"<div class='emoji'>{sender_emoji}</div>", unsafe_allow_html=True)
+    with col2:
+        st.markdown(f"<div class='{sender_class}'>{message}</div>", unsafe_allow_html=True)
+
+
+def create_prompt_template_options() -> None:
+    """Returns a drop-down widget with prompt templates."""
+    return st.selectbox(
+        '<do not display>',
+        (
+            '<Select>',
+            'Make text sound better.',
+            'Summarize text.',
+            'Code: Write doc strings',
+        ),
+    )
+
+def get_prompt_template(template_name: str) -> str:
+    """
+    Given a `template_name` get the corresponding prompt-template.
+
+    Args:
+        template_name: name of the prompt-template to get
+    """
+    return """
+            This is a prompt.
+
+            It has fields like this `{{context}}`
+
+            And this:
+
+            ```
+            {{more_context}}
+            ```
+            """
+
+
+def get_fields_from_template(prompt_template: str) -> list[str]:
+    """
+    Extracts the fields (variable names wrapped in double curly brackets) from a prompt template.
+
+    >>> template = "This is a template with these {{var_a}} and {{var_b}} as context."
+    >>> get_fields_from_template(prompt_template=template)
+    ['var_a', 'var_b']
+
+    Args:
+        prompt_template: the template to extract the fields from.
+    """
+    # TODO: unit test
+    # Regular expression pattern to match values within double brackets
+    pattern = r"\{\{(.*?)\}\}"
+    # Find all matches of the pattern in the text
+    return re.findall(pattern, prompt_template, re.DOTALL)
+
+
+def _return_mock_conversation() -> list[BaseMessage]:
+    """Returns a mock conversation with ChatGPT."""
+    message = """
+    This is some python:
+
+    ```
+    def python():
+        return True
+    ```
+
+    It is code.
+    """
+    return [
+        SystemMessage(content="You are a helpful assistant."),
+        HumanMessage(content="What is the capital of France?"),
+        AIMessage(content='The capital of France is Paris.', additional_kwargs={}, example=False),  # noqa
+        HumanMessage(content="What is 1 + 1?"),
+        AIMessage(content='2', additional_kwargs={}, example=False),  # noqa
+        HumanMessage(content="What is 2 + 2?"),
+        AIMessage(content='4', additional_kwargs={}, example=False),  # noqa
+        HumanMessage(content="What is the capital of France?"),
+        AIMessage(content='The capital of France is Paris.', additional_kwargs={}, example=False),  # noqa
+        HumanMessage(content="What is 1 + 1?"),
+        AIMessage(content='2', additional_kwargs={}, example=False),  # noqa
+        HumanMessage(content="What is 2 + 2?"),
+        AIMessage(content='4', additional_kwargs={}, example=False),  # noqa
+        HumanMessage(content="What is the capital of France?"),
+        AIMessage(content='The capital of France is Paris.', additional_kwargs={}, example=False),  # noqa
+        HumanMessage(content="What is 1 + 1?"),
+        AIMessage(content='2', additional_kwargs={}, example=False),  # noqa
+        HumanMessage(content="What is 2 + 2?"),
+        AIMessage(content=message, additional_kwargs={}, example=False),  # noqa
+    ]
+
+# import streamlit as st
+
+# def main():
+#     state = st.session_state.setdefault('state', {'text_area_b': ''})
+#     # state = {'text_area_b': ''}
+#     print(state)
+
+#     text_area_a = st.text_area("Enter text for TextArea A")
+#     button_a = st.button("Button A")
+
+#     print(f"text_area_a - 1: `{text_area_a}`")
+#     if button_a:
+#         # Update contents of TextArea B with contents of TextArea A
+#         print("updating state")
+#         state['text_area_b'] = text_area_a
+#         print(f"state: `{state['text_area_b']}`")
+
+#     print(f"state - 2: `{state['text_area_b']}`")
+#     text_area_b = st.text_area("TextArea B", value=state['text_area_b'], key='text_area_b')
+#     print(f"text_area_b - 1: `{text_area_b}`")
+
+#     button_b = st.button("Button B")
+#     if button_b:
+#         print(f"text_area_b - 2: `{text_area_b}`")
+#         st.write("Contents of TextArea B:")
+#         st.write(text_area_b)
+#         state['text_area_b'] = text_area_b  # Update session state value
+
+#     print("-----end------")
+
+# if __name__ == "__main__":
+#     main()
